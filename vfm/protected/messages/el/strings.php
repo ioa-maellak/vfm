040058d2d16fd0e01d749a1fb07efa1851a04828
melina gavriilidou <melinagavriilidou@hotmail.gr>
2015-02-19 12:53:40 +0200

    language picker extension & VFM changes
    Add language picker extension to support multiple language site
    functionality.
    layout/main.php - call language picker and yii::t()
    main.css - add div for language picker
    extension folder with language picker extension file

---
+++ vfm/protected/messages/el/strings.php
@@ -0,0 +1,17 @@
+<?php
+ 
+return array (
+    'Home' => 'Αρχική',
+    'About' => 'Σχετικά',
+    'Contact' => 'Επικοινωνία',
+    'Vehicles' => 'Οχήματα',
+    'Service' => 'Επισκευές',
+    'Shift' => 'Βάρδια',
+    'Sectors' => 'Τμήματα',
+    'Users' => 'Χρήστες',
+    'Rights' => 'Δικαιώματα',
+    'Fleet management system' => 'Σύστημα διαχείρησης οχηματων ',
+    
+);
+ 
+?>
--- vfm/protected/views/layouts/main.php
+++ vfm/protected/views/layouts/main.php
@@ -24,32 +24,39 @@
 
 	<div id="header">
 		<div id="logo"><?php echo CHtml::encode(Yii::app()->name); ?></div>
+              
 	</div><!-- header -->
-
+        <div align="right"><?php $this->widget('ext.LangPick.ELangPick', array('htmlOptions' => array(
+                    'class'=>'div.portlet-content',),));?>
+        </div>
 	<div id="mainmenu">
+                 
 		<?php 
                 $user = Yii::app()->user; 
                 $this->widget('zii.widgets.CMenu',array(
                 'items'=>array(
-           	array('label'=>'Home', 'url'=>array('/site/index')),
-                array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
-		array('label'=>'Contact', 'url'=>array('/site/contact')),
+           	array('label'=>Yii::t('strings', 'Home'), 'url'=>array('/site/index')),
+                array('label'=>Yii::t('strings', 'About'), 'url'=>array('/site/page', 'view'=>'about')),
+		array('label'=>Yii::t('strings', 'Contact'), 'url'=>array('/site/contact')),
                     
-                array('label'=>'Vehicles ', 'url'=>array('/vehicle/admin'), 'visible'=>!Yii::app()->user->isGuest),
-                array('label'=>'Service ', 'url'=>array('/vehicleService/admin'), 'visible'=>!Yii::app()->user->isGuest),
-                array('label'=>'Shift ', 'url'=>array('/vehicleShift/admin'), 'visible'=>!Yii::app()->user->isGuest),
-                array('label'=>'Sectors ', 'url'=>array('/sectorEkab/admin'), 'visible'=>!Yii::app()->user->isGuest),
+                array('label'=>Yii::t('strings', 'Vehicles'), 'url'=>array('/vehicle/admin'), 'visible'=>!Yii::app()->user->isGuest),
+                array('label'=>Yii::t('strings', 'Service'), 'url'=>array('/vehicleService/admin'), 'visible'=>!Yii::app()->user->isGuest),
+                array('label'=>Yii::t('strings', 'Shift'), 'url'=>array('/vehicleShift/admin'), 'visible'=>!Yii::app()->user->isGuest),
+                array('label'=>Yii::t('strings', 'Sectors'), 'url'=>array('/sectorEkab/admin'), 'visible'=>!Yii::app()->user->isGuest),
                
                 //main menu access to users & rights extensions
-                array('label'=>'Users', 'url'=>array('/user/admin'), 'visible'=>!Yii::app()->user->isGuest),
-                array('label'=>'Rights', 'url'=>array('/rights'), 'visible'=>!Yii::app()->user->isGuest),
+                array('label'=>Yii::t('strings', 'Users'), 'url'=>array('/user/admin'), 'visible'=>!Yii::app()->user->isGuest),
+                array('label'=>Yii::t('strings', 'Rights'), 'url'=>array('/rights'), 'visible'=>!Yii::app()->user->isGuest),
                 array('url'=>Yii::app()->getModule('user')->loginUrl, 'label'=>Yii::app()->getModule('user')->t("Login"), 'visible'=>Yii::app()->user->isGuest),
                 array('url'=>Yii::app()->getModule('user')->registrationUrl, 'label'=>Yii::app()->getModule('user')->t("Register"), 'visible'=>Yii::app()->user->isGuest),
                 array('url'=>Yii::app()->getModule('user')->profileUrl, 'label'=>Yii::app()->getModule('user')->t("Profile"), 'visible'=>!Yii::app()->user->isGuest),
                 array('url'=>Yii::app()->getModule('user')->logoutUrl, 'label'=>Yii::app()->getModule('user')->t("Logout").' ('.Yii::app()->user->name.')', 'visible'=>!Yii::app()->user->isGuest),
-		               
+		     
+               
 		),
+                
 		)); ?>
+           
 	</div><!-- mainmenu -->
 	<?php if(isset($this->breadcrumbs)):?>
 		<?php $this->widget('zii.widgets.CBreadcrumbs', array(
